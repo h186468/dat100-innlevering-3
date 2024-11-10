fba6a960613e530a90bea9e67865c7990f163779
nicolaijo10 <nuster200569@gmail.com>
2024-11-10 23:23:13 +0100

    Oppgave 2

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -4,35 +4,41 @@ import no.hvl.dat100.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
-	
-	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
-	}
+    
+    private String url;
 
-	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
-	}
-	
-	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+    // uten likes 
+    public Bilde(int id, String bruker, String dato, String tekst, String url) {
+        super(id, bruker, dato, tekst);
+        this.url = url;
+    }
 
-	}
+    // med likes
+    public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
+        super(id, bruker, dato, likes, tekst);
+        this.url = url;
+    }
 
-	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    
+    public String getUrl() {
+        return url;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	}
-
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
+    
+    @Override
+    public String toString() {
+        return "BILDE\n" + super.toString() + url + "\n";
+    }
+    
+    public String toHTML() {
 		
 		throw new UnsupportedOperationException(TODO.method());
 				
 	}
+
 }
--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -2,42 +2,45 @@ package no.hvl.dat100.oppgave2;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
+import no.hvl.dat100.oppgave1.Innlegg;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
-	
-	public Tekst () {
-		
-	}
-	
-	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+   
+    private String tekst;
 
-	}
+    //uten likes 
+    public Tekst(int id, String bruker, String dato, String tekst) {
+        super(id, bruker, dato);
+        this.tekst = tekst;
+    }
 
-	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    // med likes 
+    public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+        super(id, bruker, dato, likes);
+        this.tekst = tekst;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+  
+    public String getTekst() {
+        return tekst;
+    }
 
-	}
-	
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
-			
-		throw new UnsupportedOperationException(TODO.method());
-				
-	}
+   
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "TEKST\n" + super.toString() + tekst + "\n";
+    }
+    
+ // Metoden nedenfor er kun for valgfri oppgave 6
+ 	public String toHTML() {
+ 			
+ 		throw new UnsupportedOperationException(TODO.method());
+ 				
+ 	}
 }
